0243e923cf845009f5a23bd12f4bf1e9c6d0608a
Tom Ward <tom@popdog.net>
2023-03-16 05:56:26 +0000

    Move Sections to own module

---
+++ src/components/page/Sections.ts
@@ -0,0 +1,6 @@
+export const Sections = {
+  about: { name: "About", url: "/", enabled: true },
+  weeknotes: { name: "Weeknotes", url: "/weeknotes", enabled: true },
+  projects: { name: "Projects", url: "/projects", enabled: true },
+  articles: { name: "Articles", url: "/articles", enabled: true },
+}
--- src/lib/sections.ts
+++
@@ -1,8 +0,0 @@
-export const sections = {
-  about: { url: "/", name: "About", enabled: true },
-  weeknotes: { url: "/weeknotes", name: "Weeknotes", enabled: true },
-  projects: { url: "/projects", name: "Projects", enabled: true },
-  articles: { url: "/articles", name: "Articles", enabled: true },
-}
-
-export type Section = keyof typeof sections;
